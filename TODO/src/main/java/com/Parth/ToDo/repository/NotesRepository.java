1324e8695ab173ca460c999906f8e13fcf160d1d
Parth <94388365+praxton74@users.noreply.github.com>
2024-10-21 16:08:12 +0530

    JPA

---
+++ TODO/src/main/java/com/Parth/ToDo/repository/NotesRepository.java
@@ -0,0 +1,15 @@
+package com.Parth.TODO.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mdtalalwasim.takenote.entity.Notes;
+import com.mdtalalwasim.takenote.entity.User;
+
+public interface NotesRepository extends JpaRepository<Notes, Integer> {
+	
+	
+	List<Notes> findByUser(User user);
+
+}
